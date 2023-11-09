c9483b87ca75b3f1b67a168642bb767869b61327
Roberto <53888062+mrx6SO@users.noreply.github.com>
2023-11-08 22:15:44 -0300

    Update FitnessCards.js #1
    Ícone com a rota para acessar o componente `AdminManager`. 
    O mesmo deve ser feito para o acesso ao componente `AdminStudents`

--- components/FitnessCards.js
+++ components/FitnessCards.js
@@ -7,42 +7,79 @@ import { useNavigation } from "@react-navigation/native";
 const FitnessCards = () => {
   const FitnessData = fitness;
   const navigation = useNavigation();
+
   return (
     <View>
-      {FitnessData.map((item, key) => (
-        <Pressable
-        onPress={() => navigation.navigate("Workout",{
-          image:item.image,
-          excersises:item.excersises,
-          id:item.id,
-        })}
-          style={{ alignItems: "center", justifyContent: "center", margin: 10 }}
-          key={key}
+      {/* Botão para acessar AdminManager */}
+      <Pressable
+        onPress={() => navigation.navigate("Images")}
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          margin: 10,
+        }}
+      >
+        <Image
+          style={{ width: "95%", height: 140, borderRadius: 7 }}
+          source={{ uri: 'https://cdn-icons-png.flaticon.com/512/975/975660.png' }} 
+        />
+        <Text
+          style={{
+            position: "absolute",
+            color: "white",
+            fontSize: 16,
+            fontWeight: "bold",
+            left: 20,
+            top: 20,
+          }}
         >
-          <Image
-            style={{ width: "95%", height: 140, borderRadius: 7 }}
-            source={{ uri: item.image }}
-          />
-          <Text
-            style={{
-              position: "absolute",
-              color: "white",
-              fontSize: 16,
-              fontWeight: "bold",
-              left: 20,
-              top: 20,
-            }}
-          >
-            {item.name}
-          </Text>
-          <MaterialCommunityIcons
-            style={{ position: "absolute", color: "white", bottom: 15,left:20 }}
-            name="lightning-bolt"
-            size={24}
-            color="black"
-          />
-        </Pressable>
-      ))}
+          Admin Manager
+        </Text>
+        <MaterialCommunityIcons
+          style={{ position: "absolute", color: "white", bottom: 15, left: 20 }}
+          name="lightning-bolt"
+          size={24}
+          color="black"
+        />
+      </Pressable>
+    {FitnessData.map((item, key) => (
+      <Pressable
+      onPress={() => navigation.navigate("Workout",{
+        image:item.image,
+        excersises:item.excersises,
+        id:item.id,
+      })}
+        style={{ alignItems: "center", justifyContent: "center", margin: 10 }}
+        key={key}
+      >
+        <Image
+          style={{ width: "95%", height: 140, borderRadius: 7 }}
+          source={{ uri: item.image }}
+        />
+        <Text
+          style={{
+            position: "absolute",
+            color: "white",
+            fontSize: 16,
+            fontWeight: "bold",
+            left: 20,
+            top: 20,
+          }}
+        >
+          {item.name}
+        </Text>
+        <MaterialCommunityIcons
+          style={{ position: "absolute", color: "white", bottom: 15,left:20 }}
+          name="lightning-bolt"
+          size={24}
+          color="black"
+        />
+      </Pressable>
+    ))}
+ 
+      
+
+      
     </View>
   );
 };
